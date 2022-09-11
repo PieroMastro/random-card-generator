d0e4639e0cbff34053bfd35146eb2505e740ec9e
PieroMastro <pier.andres@gmail.com>
2022-09-11 04:56:56 +0000

    HTML, CSS and JavsScript done. Missing color change for suits

--- src/app.js
+++ src/app.js
@@ -5,7 +5,35 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+const suit = ["♦", "♥", "♠", "♣"];
+const numbers = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A"
+];
+
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let randomSuit = Math.floor(Math.random() * suit.length);
+  let randomNumber = Math.floor(Math.random() * numbers.length);
+
+  let suitTop = document.querySelector(".suit-top");
+  suitTop.innerHTML = suit[randomSuit];
+
+  let suitBottom = document.querySelector(".suit-bottom");
+  suitBottom.innerHTML = suit[randomSuit];
+
+  let number = document.querySelector(".number");
+  number.innerHTML = numbers[randomNumber];
 };
