3fa05c2038ee91474b035c9239f39dc0936ff188
PieroMastro <pier.andres@gmail.com>
2022-09-12 21:29:36 +0000

    add a button

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-const suit = ["♦", "♥", "♠", "♣"];
+const suits = ["♦", "♥", "♠", "♣"];
 const numbers = [
   "2",
   "3",
@@ -22,18 +22,21 @@ const numbers = [
   "A"
 ];
 
-window.onload = () => {
+window.onload = function() {
   //write your code here
+  let randomSuit = Math.floor(Math.random() * suits.length);
 
-  let randomSuit = Math.floor(Math.random() * suit.length);
   let randomNumber = Math.floor(Math.random() * numbers.length);
 
   let suitTop = document.querySelector(".suit-top");
-  suitTop.innerHTML = suit[randomSuit];
+  suitTop.innerHTML = suits[randomSuit];
 
   let suitBottom = document.querySelector(".suit-bottom");
-  suitBottom.innerHTML = suit[randomSuit];
+  suitBottom.innerHTML = suits[randomSuit];
 
-  let number = document.querySelector(".number");
-  number.innerHTML = numbers[randomNumber];
+  let cardValue = document.querySelector(".numbers");
+  cardValue.innerHTML = numbers[randomNumber];
 };
+
+//if ♥️ || ♦️ change color to red <----- pendiente
+//agregar boton para seleccionar una carta al azar (si hay tiempo)
